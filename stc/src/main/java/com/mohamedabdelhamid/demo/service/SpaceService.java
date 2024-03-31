747f72816d7b6d2aa42a224343ed7ffce4353834
AceAlBastoni <elhlawy@gmail.com>
2024-03-31 04:42:21 +0200

    Add .metadata folder to .gitignore

---
+++ stc/src/main/java/com/mohamedabdelhamid/demo/service/SpaceService.java
@@ -0,0 +1,48 @@
+package com.mohamedabdelhamid.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mohamedabdelhamid.demo.domains.PermissionGroup;
+import com.mohamedabdelhamid.demo.domains.Space;
+import com.mohamedabdelhamid.demo.domains.User;
+import com.mohamedabdelhamid.demo.dto.UserDto;
+import com.mohamedabdelhamid.demo.repository.PermissionGroupRepository;
+import com.mohamedabdelhamid.demo.repository.SpaceRepository;
+
+//SpaceService.java
+@Service
+public class SpaceService {
+ @Autowired
+ private SpaceRepository spaceRepository;
+ 
+ @Autowired
+ private PermissionGroupRepository permissionGroupRepository;
+ 
+ @Transactional
+ public Space createSpace(String spaceName, String groupName, List<UserDto> users) {
+     // Create permission group
+     PermissionGroup permissionGroup = new PermissionGroup();
+     permissionGroup.setName(groupName);
+     
+     for (UserDto userDto : users) {
+         User user = new User();
+         user.setEmail(userDto.getEmail());
+         user.setAccessLevel(userDto.getAccessLevel());
+         user.setPermissionGroup(permissionGroup);
+         permissionGroup.getUsers().add(user);
+     }
+     
+     permissionGroup = permissionGroupRepository.save(permissionGroup);
+     
+     // Create space
+     Space space = new Space();
+     space.setName(spaceName);
+     space.setPermissionGroup(permissionGroup);
+     
+     return spaceRepository.save(space);
+ }
+}
